e77f832e92604ca363ae976db71349f81e636a1c
unknown <sergioandujar7@gmail.com>
2024-03-04 10:53:10 +0100

    first version

--- tests/taskManager.test.ts
+++ tests/taskManager.test.ts
@@ -8,18 +8,18 @@ describe("TaskManager", () => {
     });
 
     it("debería agregar una task a la lista", () => {
-        const task = taskManager.addTask("Do laundry");
+        const task = taskManager.addTask("Comprar manzanas");
         expect(taskManager.getTasks()).toContain(task);
     });
 
     it("debería marcar una task como completada", () => {
-        const task = taskManager.addTask("Do laundry");
+        const task = taskManager.addTask("Comprar manzanas");
         taskManager.markTaskAsCompleted(task.id);
         expect(task.completed).toBe(true);
     });
 
     it("debería borrar una task de la lista", () => {
-        const task = taskManager.addTask("Do laundry");
+        const task = taskManager.addTask("Comprar manzanas");
         taskManager.removeTask(task.id);
         expect(taskManager.getTasks()).not.toContain(task);
     });
